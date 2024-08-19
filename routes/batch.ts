22ec6efe4d510511212957ea092b928ca2ba3ebf
DILG Programmer <juderibleza36@gmail.com>
2024-08-19 09:26:58 +0800

    chasdas

--- routes/batch.ts
+++ routes/batch.ts
@@ -1,28 +1,28 @@
-import express,{Request, Response} from "express"
-import { prisma } from "../src/config/prisma"
-const router = express.Router()
+// import express,{Request, Response} from "express"
+// import { prisma } from "../src/config/prisma"
+// const router = express.Router()
 
-router.post("/new", async(req: Request, res:Response)=>{
-    try {
-        const batchYear = req.body
-        await prisma.batchYear.create({data: batchYear})
-        res.status(200).json({message: "OK"})
-    } catch (error) {
-        console.log(error);
-    }finally{
-        await prisma.$disconnect()
-    }
-})
+// router.post("/new", async(req: Request, res:Response)=>{
+//     try {
+//         const batchYear = req.body
+//         await prisma.batchYear.create({data: batchYear})
+//         res.status(200).json({message: "OK"})
+//     } catch (error) {
+//         console.log(error);
+//     }finally{
+//         await prisma.$disconnect()
+//     }
+// })
 
-router.get("/list", async(req: Request, res:Response)=>{
-    try {
-        const data = await prisma.batchYear.findMany()
-        res.status(200).json(data)
-    } catch (error) {
-        console.log(error);
-    }finally{
-        await prisma.$disconnect()
-    }
-})
+// router.get("/list", async(req: Request, res:Response)=>{
+//     try {
+//         const data = await prisma.batchYear.findMany()
+//         res.status(200).json(data)
+//     } catch (error) {
+//         console.log(error);
+//     }finally{
+//         await prisma.$disconnect()
+//     }
+// })
 
-export default router
+// export default router
