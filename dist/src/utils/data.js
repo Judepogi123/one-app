917ab3c22f1dcb31301a789f4d7874314845ca83
DILG Programmer <juderibleza36@gmail.com>
2024-08-19 09:45:41 +0800

    checked

--- dist/src/utils/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleGenTagID = void 0;
-const handleGenTagID = (min = 100000, max = 999999) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-exports.handleGenTagID = handleGenTagID;
-// Example usage:
