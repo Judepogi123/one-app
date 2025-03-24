249a28123e5f8240a64622137bb507dafce43c06
Judepogi123 <juderibleza36@gmail.com>
2025-03-24 21:59:16 +0800

    Server live 1.9

--- dist/src/routes/image.js
+++ dist/src/routes/image.js
@@ -130,9 +130,9 @@ router.post('/generate-id', (req, res) => __awaiter(void 0, void 0, void 0, func
                     });
                     voterQR = (qrCodeData === null || qrCodeData === void 0 ? void 0 : qrCodeData.qrCode) || '';
                 }
-                const bcImage = yield (0, canvas_1.loadImage)(path_1.default.join(__dirname, '/public/images/bc.png'));
-                const pcImage = yield (0, canvas_1.loadImage)(path_1.default.join(__dirname, '/public/images/pc.png'));
-                const tlImage = yield (0, canvas_1.loadImage)(path_1.default.join(__dirname, '/public/images/tl.png'));
+                const bcImage = yield (0, canvas_1.loadImage)(path_1.default.join(__dirname, '/src/public/images/bc.png'));
+                const pcImage = yield (0, canvas_1.loadImage)(path_1.default.join(__dirname, '/src/public/images/pc.png'));
+                const tlImage = yield (0, canvas_1.loadImage)(path_1.default.join(__dirname, '/src/public/images/tl.png'));
                 const iamgeList = [tlImage, tlImage, pcImage, bcImage];
                 ctx.drawImage(iamgeList[headerLevel], 0, 0, width, height);
                 if (voterQR) {
